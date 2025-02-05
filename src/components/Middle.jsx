476e3884e6b0217d17d90987f2228cbc5aea5f82
Axel Ahlander <axel.ahlander@gmail.com>
2025-02-05 14:54:00 +0100

    ext 1

--- src/components/Middle.jsx
+++ src/components/Middle.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import CreateTweet from './CreateTweet'
 
 function Middle(props) {
   return (
@@ -7,37 +8,7 @@ function Middle(props) {
                     <h2 className="title">Home</h2>
                 </div>
 
-                <div className='create-tweet'>
-                    <form onSubmit={props.addTweet}>
-                        <div className="avatar-section">
-                            <div className="profile-icon"><img src={props.loggedInUser.profileImage}/></div>
-                        </div>
-
-                        <div className="textarea-section">
-                            <textarea
-                                className="content"
-                                type="text"
-                                placeholder="What is happening?!"
-                                value={props.createTweetContent}
-                                onChange={(e) => props.setCreateTweetContent(e.target.value)}
-                            ></textarea>
-                        </div>
-
-                        <div></div>
-
-                        <div className="actions-section">
-                            <div className="actions">
-                                <i className="fa-regular fa-image action-icon"></i>
-                                <i className="fa-solid fa-list action-icon"></i>
-                                <i className="fa-regular fa-face-smile action-icon"></i>
-                                <i className="fa-regular fa-calendar action-icon"></i>
-                                <i className="fa-solid fa-location-dot action-icon"></i>
-                            </div>
-
-                            <button type="submit" disabled={props.createTweetContent.length < 1} className="tweet-btn">Tweet</button>
-                        </div>
-                    </form>
-                </div>
+                <CreateTweet addTweet={props.addTweet} loggedInUser={props.loggedInUser} setCreateTweetContent={props.setCreateTweetContent} createTweetContent={props.createTweetContent} />
 
                 <div className="show-more-tweets">
                     <p>Show 35 Tweets</p>
